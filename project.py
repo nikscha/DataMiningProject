2fa04c909c606f95cdc27ac63f79e4d1edafccf5
Gerritsen, B.A. (Ben) <ben.gerritsen@student.ru.nl>
2020-01-07 15:17:20 +0100

    i pooped

--- project.py
+++ project.py
@@ -5,4 +5,7 @@ Created on Tue Jan  7 14:53:15 2020
 @author: nikscha
 """
 
-print("hey ben")
+import pandas as pd
+gf = pd.read_csv('steam-data-master/analysis/games-features-edit.csv')
+print(gf.columns)
+gf.head()
