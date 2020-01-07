2a225483e46936000a947a46e1e59bc49b423be5
nikscha <dastaurin@gmail.com>
2020-01-07 15:19:03 +0100

    nothing

--- project.py
+++ project.py
@@ -6,3 +6,15 @@ Created on Tue Jan  7 14:53:15 2020
 """
 
 print("hey ben")
+import scipy as scipy
+from scipy import stats
+from scipy import io
+import matplotlib.pyplot as plt
+import numpy as numpy
+import pandas as pd
+
+#data=io.loadmat('steam-data-master\analysis\games-features-edit.csv')
+from numpy import genfromtxt
+gf = pd.read_csv('steam-data-master\analysis\games-features-edit.csv')
+print(gf.columns)
+gf.head()
